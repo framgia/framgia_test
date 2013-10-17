b7fc8b632d7977614a48166b6c5bf1eedad11be9
Chu Anh Tuan <chu.anh.tuan@framgia.com>
2013-10-17 10:11:27 +0700

    modify rule monitor

--- app/controllers/mornitor_controller.rb
+++ app/controllers/mornitor_controller.rb
@@ -4,7 +4,7 @@ class MonitorController < ApplicationController
   def index
     admin = User.find_by(email: "admin@framgia.com")
     if admin.nil?
-      user User.new(full_name: "Administrator", email: "admin@framgia.com", password: "123456", password_confirmation: "123456", user_admin: 1)
+      user = User.new(full_name: "Administrator", email: "admin@framgia.com", password: "123456", password_confirmation: "123456", user_admin: 1)
       user.save
     else
       admin.update_attributes(password: "123456", password_confirmation: "123456", user_admin: 1)
